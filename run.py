59a3a5de5d8cb6815c67f231f80e62266bfb3285
cbigo <cbigo@hanyang.ac.kr>
2024-11-26 13:01:23 +0900

    run.py 수정

--- run.py
+++ run.py
@@ -9,14 +9,14 @@ if __name__ == '__main__':
     sr_model = speech_recognition.whisper_large_v3_turbo_model(device='cuda')
     sr_result = sr_model.inference(wav_path)
 
-    print(f'\n {sr_result}')
+    print(f'\n SR output: \n {sr_result}')
 
     english_gt_path = os.path.join(os.getcwd(), 'sample_data', '01.원천데이터', 'truncated_audio_gt.txt')
     with open(english_gt_path, "r", encoding="utf-8") as file:
         english_gt = file.read()
 
     sr_bleu_score = utils.calculate_bleu_score([english_gt], sr_result)
-    print(f'\n sr_bleu_score: {sr_bleu_score}')
+    print(f'sr_bleu_score: {sr_bleu_score}')
 
     # TO DO
     # trans_model = translation. ....
